751fda993c4556d3ab7d0be45242bb9df7d421fd
Juan camilo <61300748+bichozx@users.noreply.github.com>
2025-09-19 23:55:21 -0500

    se deja funcional el post
    se organizan los post para que el likes y el comments funcionen

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,125 +1,184 @@
-import React from 'react';
+import React, { useState } from "react";
 
 export const Post = () => {
+  const [posts, setPosts] = useState([
+    {
+      id: 1,
+      user: "John Doe",
+      avatar: "https://www.w3schools.com/w3images/avatar2.png",
+      time: "1 min",
+      text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit...",
+      images: [
+        "https://www.w3schools.com/w3images/lights.jpg",
+        "https://www.w3schools.com/w3images/nature.jpg",
+      ],
+      likes: 0,
+      comments: [],
+    },
+    {
+      id: 2,
+      user: "Jane Doe",
+      avatar: "https://www.w3schools.com/w3images/avatar5.png",
+      time: "16 min",
+      text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit...",
+      images: [],
+      likes: 0,
+      comments: [],
+    },
+    {
+      id: 3,
+      user: "Angie Jane",
+      avatar: "https://www.w3schools.com/w3images/avatar6.png",
+      time: "32 min",
+      text: "Have you seen this? Lorem ipsum dolor sit amet...",
+      images: ["https://www.w3schools.com/w3images/nature.jpg"],
+      likes: 0,
+      comments: [],
+    },
+  ]);
+
+  const [newPost, setNewPost] = useState("");
+  const [newComment, setNewComment] = useState({}); 
+
+  const handleAddPost = () => {
+    if (!newPost.trim()) return;
+    const post = {
+      id: posts.length + 1,
+      user: "You",
+      avatar: "https://www.w3schools.com/w3images/avatar2.png",
+      time: "Just now",
+      text: newPost,
+      images: [],
+      likes: 0,
+      comments: [],
+    };
+    setPosts([post, ...posts]);
+    setNewPost("");
+  };
+
+  const handleLike = (id) => {
+    setPosts(
+      posts.map((p) =>
+        p.id === id ? { ...p, likes: p.likes + 1 } : p
+      )
+    );
+  };
+
+  const handleAddComment = (postId) => {
+    const commentText = newComment[postId]?.trim();
+    if (!commentText) return;
+
+    setPosts(
+      posts.map((p) =>
+        p.id === postId
+          ? { ...p, comments: [...p.comments, { text: commentText, user: "You" }] }
+          : p
+      )
+    );
+
+   
+    setNewComment({ ...newComment, [postId]: "" });
+  };
+
   return (
     <>
+      {/* Formulario para crear un post */}
       <div className="w3-row-padding">
         <div className="w3-col m12">
           <div className="w3-card w3-round w3-white">
             <div className="w3-container w3-padding">
-              <h6 className="w3-opacity">Social Media template by w3.css</h6>
-              <p contenteditable="true" className="w3-border w3-padding">
-                Status: Feeling Blue
-              </p>
-              <button type="button" className="w3-button w3-theme">
-                <i className="fa fa-pencil"></i>Post
+              <h6 className="w3-opacity">Create a post</h6>
+              <textarea
+                className="w3-input w3-border w3-padding"
+                placeholder="What's on your mind?"
+                value={newPost}
+                onChange={(e) => setNewPost(e.target.value)}
+              />
+              <button
+                type="button"
+                className="w3-button w3-theme w3-margin-top"
+                onClick={handleAddPost}
+              >
+                <i className="fa fa-pencil"></i> Post
               </button>
             </div>
           </div>
         </div>
       </div>
 
-      <div className="w3-container w3-card w3-white w3-round w3-margin">
-        <br />
-        <img
-          src="https://www.w3schools.com/w3images/avatar2.png"
-          alt="Avatar"
-          className="w3-left w3-circle w3-margin-right"
-          style={{"width":"60px"}}
-        />
-        <span className="w3-right w3-opacity">1 min</span>
-        <h4>John Doe</h4>
-        <br />
-        <hr className="w3-clear" />
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat.
-        </p>
-        <div className="w3-row-padding" style={{"margin":"0 -16px"}}>
-          <div className="w3-half">
-            <img
-              src="https://www.w3schools.com/w3images/lights.jpg"
-              style={{"width":"100%"}}
-              alt="Northern Lights"
-              className="w3-margin-bottom"
+      {/* Lista de posts */}
+      {posts.map((post) => (
+        <div
+          key={post.id}
+          className="w3-container w3-card w3-white w3-round w3-margin"
+        >
+          <br />
+          <img
+            src={post.avatar}
+            alt="Avatar"
+            className="w3-left w3-circle w3-margin-right"
+            style={{ width: "100px" }}
+          />
+          <span className="w3-right w3-opacity">{post.time}</span>
+          <h4>{post.user}</h4>
+          <br />
+          <hr className="w3-clear" />
+          <p>{post.text}</p>
+
+          {/* Imágenes del post */}
+          {post.images.length > 0 && (
+            <div className="w3-row-padding" style={{ margin: "0 -16px" }}>
+              {post.images.map((img, index) => (
+                <div className="w3-half" key={index}>
+                  <img
+                    src={img}
+                    alt="Post media"
+                    style={{ width: "100%" }}
+                    className="w3-margin-bottom"
+                  />
+                </div>
+              ))}
+            </div>
+          )}
+
+          {/* Botones */}
+          <button
+            type="button"
+            className="w3-button w3-theme-d1 w3-margin-bottom"
+            onClick={() => handleLike(post.id)}
+          >
+            <i className="fa fa-thumbs-up"></i> Like ({post.likes})
+          </button>
+
+          {/* Sección de comentarios */}
+          <div className="w3-margin-top">
+            <input
+              type="text"
+              className="w3-input w3-border"
+              placeholder="Write a comment..."
+              value={newComment[post.id] || ""}
+              onChange={(e) =>
+                setNewComment({ ...newComment, [post.id]: e.target.value })
+              }
             />
-          </div>
-          <div className="w3-half">
-            <img
-              src="https://www.w3schools.com/w3images/nature.jpg"
-              style={{"width":"100%"}}
-              alt="Nature"
-              className="w3-margin-bottom"
-            />
+            <button
+              className="w3-button w3-theme w3-small w3-margin-top"
+              onClick={() => handleAddComment(post.id)}
+            >
+              Comment
+            </button>
+
+            {/* Lista de comentarios */}
+            <div className="w3-margin-top">
+              {post.comments.map((c, i) => (
+                <p key={i}>
+                  <strong>{c.user}:</strong> {c.text}
+                </p>
+              ))}
+            </div>
           </div>
         </div>
-        <button type="button" className="w3-button w3-theme-d1 w3-margin-bottom">
-          <i className="fa fa-thumbs-up"></i>Like
-        </button>
-        <button type="button" className="w3-button w3-theme-d2 w3-margin-bottom">
-          <i className="fa fa-comment"></i>Comment
-        </button>
-      </div>
-
-      <div className="w3-container w3-card w3-white w3-round w3-margin">
-        <br />
-        <img
-          src="https://www.w3schools.com/w3images/avatar5.png"
-          alt="Avatar"
-          className="w3-left w3-circle w3-margin-right"
-          style={{"width":"60px"}}
-        />
-        <span className="w3-right w3-opacity">16 min</span>
-        <h4>Jane Doe</h4>
-        <br />
-        <hr className="w3-clear" />
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat.
-        </p>
-        <button type="button" className="w3-button w3-theme-d1 w3-margin-bottom">
-          <i className="fa fa-thumbs-up"></i>Like
-        </button>
-        <button type="button" className="w3-button w3-theme-d2 w3-margin-bottom">
-          <i className="fa fa-comment"></i>Comment
-        </button>
-      </div>
-
-      <div className="w3-container w3-card w3-white w3-round w3-margin">
-        <br />
-        <img
-          src="https://www.w3schools.com/w3images/avatar6.png"
-          alt="Avatar"
-          className="w3-left w3-circle w3-margin-right"
-          style={{"width":"60px"}}
-        />
-        <span className="w3-right w3-opacity">32 min</span>
-        <h4>Angie Jane</h4>
-        <br />
-        <hr className="w3-clear" />
-        <p>Have you seen this?</p>
-        <img
-          src="https://www.w3schools.com/w3images/nature.jpg"
-          style={{"width":"100%"}}
-          className="w3-margin-bottom"
-        />
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat.
-        </p>
-        <button type="button" className="w3-button w3-theme-d1 w3-margin-bottom">
-          <i className="fa fa-thumbs-up"></i>Like
-        </button>
-        <button type="button" className="w3-button w3-theme-d2 w3-margin-bottom">
-          <i className="fa fa-comment"></i>Comment
-        </button>
-      </div>
+      ))}
     </>
   );
 };
